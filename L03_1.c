98553bb92ca0d62686a1ec481534c992f828efcd
Aleksandar <39736298+Tula99@users.noreply.github.com>
2019-03-20 18:38:54 +0100

    Update L03_1.c

--- L03_1.c
+++ L03_1.c
@@ -6,29 +6,30 @@
 char *formiraj(int n, ...)
 {
     int i;va_list v; va_start(v, n);
-    char *prenos = 0;
-    int duzina = 0;
+    char *prenos = 0; /* DEKLARISEM STRING KOJI CU VRATITI KAO REZ. */
+    int duzina = 0; /* DUZINA TOG STRINGA */
 
     for (i=0;i<n;i++)
     {
-        int broj = va_arg(v, int);
+        int broj = va_arg(v, int); /* UZIMAM PRVI BROJ */
 
         if ( broj < 0 ) { prenos = (char*)realloc(prenos, sizeof(char) * duzina + 1 + 1); *(prenos + duzina) = '-'; duzina++; }
+        /* POSTO SU CIJELI BROJEVI MOGU BITI NEGATIVNI */
 
-        char broj_c[100];
+        char broj_c[100]; /* SKLADISTIM BROJ KONVERTOVAN U STRING */
         itoa(abs(broj), broj_c, 10);
 
         int  j = 0;
-        while (broj_c[j]) { j++; }
+        while (broj_c[j]) { j++; } /* GLEDAM KOLIKO CIFARA IMA BROJ */
 
-        prenos = (char*)realloc(prenos, sizeof(char) * duzina + j + 1);
+        prenos = (char*)realloc(prenos, sizeof(char) * duzina + j + 1); /* MIJENJM DIMENZIJE STRINGA U KOJI CU SMJESTITI BROJ(STRING) */
 
         int k = 0;
-        for (k=0;k<j;k++) *(prenos + duzina + k) = broj_c[k];
+        for (k=0;k<j;k++) *(prenos + duzina + k) = broj_c[k]; /* DODJELJUJEM STRING ODGOVARAJUCM POZICIJAMA */
         duzina+=j;
     }
 
-    *(prenos + duzina) = 0;
+    *(prenos + duzina) = 0; /* NULA OPERATOR PREKIDA STRING */
 
     return prenos;
 }
