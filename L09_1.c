c32da5e9c3d629f31ee8afe1e5149d26c68f0b1c
Aleksandar <39736298+Tula99@users.noreply.github.com>
2019-05-23 17:37:42 +0200

    Update L09_1.c

--- L09_1.c
+++ L09_1.c
@@ -6,28 +6,42 @@
 #include <windows.h>
 
 typedef struct proces{
-    char naziv[50];
-    int broj_s;
+    char naziv[20];
+    int broj;
 }PROCES;
 
 typedef struct cvor{
-    PROCES data;
+    PROCES p;
     struct cvor *next;
 }CVOR;
 
-void add(CVOR **top, CVOR **bottom, PROCES podatak)
+void dodaj(CVOR **pf, CVOR **pr, PROCES *p)
 {
     CVOR *novi = (CVOR*)malloc(sizeof(CVOR));
 
-    novi->data = podatak;
+    novi->p = *p;
     novi->next = 0;
 
-    if ( *top == 0 ) *top = *bottom = novi;
+    if ( *pf == 0 ) *pf = *pr = novi;
     else
     {
-        (*bottom)->next = novi;
-        *bottom = novi;
+        (*pr)->next = novi;
+        *pr = novi;
     }
+}
+
+int brisi(CVOR **pf, CVOR **pr, PROCES *p)
+{
+    if ( *pf == 0 ) return 0;
+
+    CVOR *buff = *pf;
+    *p = buff->p;
+
+    if ( *pf == *pr ) *pf = *pr = 0;
+    else *pf = buff->next;
+
+    free(buff);
+    return 1;
 }
 
 int main()
@@ -38,11 +52,11 @@ int main()
     printf("Unesite broj procesa: ");
     scanf("%d", &n);
 
-    CVOR *top = 0;
-    CVOR *bottom = 0;
+    CVOR *pf = 0;
+    CVOR *pr = 0;
 
-    int i = 0;
-    for (i=1;i<=n;i++)
+    int i = 1;
+    for (;i<=n;i++)
     {
         char naziv[50] = "proces";
         int broj = rand() % 5 + 1;
@@ -51,15 +65,32 @@ int main()
         itoa(i, br, 10);
         strcat(naziv, br);
 
-        PROCES podatak;
-        strcpy(podatak.naziv, naziv);
-        podatak.broj_s = broj;
+        PROCES p;
+        strcpy(p.naziv, naziv);
+        p.broj = broj;
 
-        add(&top, &bottom, podatak);
+        dodaj(&pf, &pr, &p);
 
         printf("naziv_procesa: %s; vrijeme: %ds\n", naziv, broj);
     }
 
-    Sleep(3000); //NIJE DOVRSENO NE KONTAM STA TREBA URADITI
+    char ins[100];
+    printf("Unesite naredbu: ");
+    scanf("%s", ins);
+
+    if ( !strcmp(ins, "START") )
+    {
+        PROCES p;
+        while ( pf )
+        {
+            Sleep(pf->p.broj*1000);
+
+            printf("naziv_procesa: %s; vrijeme: %ds\n", pf->p.naziv, pf->p.broj);
+
+            brisi(&pf, &pr, &p);
+
+            printf("Izbrisan proce: %s; vrijeme: %ds\n\n", p.naziv, p.broj);
+        }
+    }
     return 0;
 }
